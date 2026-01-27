66236544053befd05db7a9d3c528a2146f1dc8e4
PRATIK MISHRA <158600792+pratikmishra09@users.noreply.github.com>
2026-01-27 14:40:24 +0530

    Add files via upload
    New file add Array

---
+++ Array.js
@@ -0,0 +1,16 @@
+// let marks = [200, 987 , "Pratik",true, 22 ];
+// marks.pop("true",22);
+// //marks.push("Mishra", true , false , Boolean);
+// console.log(marks);
+// marks.splice(true , 22 );
+// console.log(marks);
+
+// Checking the primitive data vs non primitive data 
+
+let a = "pratik";
+let b = a;
+let arr = [09 , "pratik", true];
+console.log(b,arr);
+
+let arr3 = [34,69,367,698439,];
+console.log(arr3);
