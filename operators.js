0706b1b85c8f669865aaae3561c2d54df5994fb8
PRATIK MISHRA <158600792+pratikmishra09@users.noreply.github.com>
2026-01-31 22:48:30 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,25 @@
+// Arithmetic Operators 
+let a = 10; 
+let b = 20;
+
+let c = a+b;
+console.log(c);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+
+// new thing start form hear
+
+let l = "98";
+let m = Number(l)
+console.log(m);
+
+
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+
+
+let agea = 23;
+agea + 2;
+console.log(agea);
