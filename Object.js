ab6e2e665c70a574ffac680bc261a6581518f101
PRATIK MISHRA <158600792+pratikmishra09@users.noreply.github.com>
2026-02-08 17:45:13 +0530

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,15 @@
+// let student = {
+//  name : "pratik",
+//  Age : 20, 
+//  "roll no ": 1033
+// }
+
+// // let arr = ["name ", 26 , 22, ]
+
+// // console.log(arr);
+// console.log(student.name);
+
+// Generate a random number between 1 to 100
+
+const random = Math.floor(Math.random()*100)+1;
+console.log(random)
